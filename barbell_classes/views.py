683be31907bf5ffc974e4381a4c0054a8ccba051
Joel Chan <ic.chanjoel@gmail.com>
2024-05-22 11:37:55 +0000

    add content to views page

--- barbell_classes/views.py
+++ barbell_classes/views.py
@@ -1,10 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+barbellclass_updates = [
+    {
+        'author': 'Joel',
+        'title': 'Barbell Class 1',
+        'content': 'First class content',
+        'date_posted': 'May 10, 2024'
+    },
+    {
+        'author': 'Carl',
+        'title': 'Barbell Class 2',
+        'content': 'Second class content',
+        'date_posted': 'May 11, 2024'
+    }
+]
 
 def home(request):
-    return render(request, 'barbell_classes/home.html')
+    context = {
+        'barbellclass_updates': barbellclass_updates
+    }
+    return render(request, 'barbell_classes/home.html', context)
 
 
 def timetable(request):
-    return HttpResponse('barbell_classes/timetable.html')
+    return render(request, 'barbell_classes/timetable.html')

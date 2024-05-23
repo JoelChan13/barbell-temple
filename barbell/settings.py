9f28d33851bf775027c724969765cfc5fdb6820d
Joel Chan <ic.chanjoel@gmail.com>
2024-05-23 16:05:28 +0000

    add crispy forms to the app

--- barbell/settings.py
+++ barbell/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = ['8000-joelchan13-barbelltempl-2a6iqe2iaen.ws-eu114.gitpod.io','
 INSTALLED_APPS = [
     'barbell_classes.apps.BarbellClassesConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
+    'crispy_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'

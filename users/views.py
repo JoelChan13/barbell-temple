91cc61e08d3911bfccc17e4f118ca6650d19bc5b
Joel Chan <ic.chanjoel@gmail.com>
2024-05-23 10:32:02 +0000

    update registration view to validate form data if POST request received

--- users/views.py
+++ users/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def registration(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account Created for Templar {username}!')
+            return redirect('barbell_classes-home')
+    else:
+        form = UserCreationForm()
     return render(request, 'users/registration.html', {'form': form})

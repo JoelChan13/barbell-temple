f9951f8e6fc925f845e695304c02349805a4a512
Joel Chan <ic.chanjoel@gmail.com>
2024-05-23 15:30:05 +0000

    update registration page to include more data when signing up

--- users/views.py
+++ users/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegistrationForm
 
 
 def registration(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account Created for Templar {username}!')
             return redirect('barbell_classes-home')
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
     return render(request, 'users/registration.html', {'form': form})

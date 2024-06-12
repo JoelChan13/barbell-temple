348cb3a3775e0a8971ea0bde295f22d163173049
Joel Chan <ic.chanjoel@gmail.com>
2024-06-12 09:57:40 +0000

    modify the url for static files in settings file

--- barbell/settings.py
+++ barbell/settings.py
@@ -131,8 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = '/static/'
+
+# Point to the static directory inside the barbell_classes app
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'barbell_classes', 'static')]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

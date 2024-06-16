a2f29a5992d9463d32295ed3f073aa931ecf6646
Joel Chan <ic.chanjoel@gmail.com>
2024-06-16 13:25:27 +0000

    add page header in home page

--- barbell_classes/admin.py
+++ barbell_classes/admin.py
@@ -4,24 +4,28 @@ from .models import BarbellClass
 class BarbellClassAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'date_posted')
     readonly_fields = ('author', 'date_posted')
-    
+
+
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.is_superuser:
             return qs
-        return qs.filter(author=request.user)
-    
-    def has_change_permission(self, request, obj=None):
+        return qs.filter(author = request.user)
+
+
+    def has_change_permission(self, request, obj = None):
         if obj is not None and request.user == obj.author:
             return True
-        return super().has_change_permission(request, obj=obj)
-    
-    def has_delete_permission(self, request, obj=None):
+        return super().has_change_permission(request, obj = obj)
+
+
+    def has_delete_permission(self, request, obj = None):
         if obj is not None and request.user == obj.author:
             return True
-        return super().has_delete_permission(request, obj=obj)
-    
-    def get_readonly_fields(self, request, obj=None):
+        return super().has_delete_permission(request, obj = obj)
+
+
+    def get_readonly_fields(self, request, obj = None):
         if request.user.is_superuser:
             return []
         return ['author', 'date_posted']
--- barbell_classes/models.py
+++ barbell_classes/models.py
@@ -11,18 +11,21 @@ class BarbellClass(models.Model):
         ('Advanced', 'Advanced'),
     ]
 
-    title = models.CharField(max_length=100, help_text="Enter the title of the barbell class (e.g., 'Beginner Strength Training')")
-    image = CloudinaryField('image', default='placeholder')
-    date_posted = models.DateTimeField(default=timezone.now, editable=False)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    class_datetime = models.DateTimeField(default=timezone.now, help_text="Enter the date and time of the class (yyyy/mm/dd hours:minutes)")
-    duration = models.TextField(default='', help_text="Enter the duration of the class (e.g., '1 hour')")
-    difficulty = models.CharField(max_length=20, choices=DIFFICULTY_CHOICES, default='Beginner', help_text="Select the difficulty level of the class")
+    title = models.CharField(max_length = 100, help_text = "Enter the title of the barbell class (e.g., 'Beginner Strength Training')")
+    image = CloudinaryField('image', default = 'placeholder')
+    date_posted = models.DateTimeField(default = timezone.now, editable = False)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    class_datetime = models.DateTimeField(default = timezone.now, help_text = "Enter the date and time of the class (yyyy/mm/dd hours:minutes)")
+    duration = models.TextField(default = '', help_text = "Enter the duration of the class (e.g., '1 hour')")
+    difficulty = models.CharField(max_length = 20, choices = DIFFICULTY_CHOICES, default = 'Beginner', 
+help_text = "Select the difficulty level of the class")
     description = models.TextField(help_text="Provide a detailed description of the class")
-    available_spots = models.IntegerField(default=0, help_text="Number of available spots")
+    available_spots = models.IntegerField(default = 0, help_text = "Number of available spots")
+
 
     def __str__(self):
         return self.title
+
 
     def get_absolute_url(self):
         return reverse('barbellclass-detail', kwargs={'pk': self.pk})
@@ -35,6 +38,7 @@ class Enrollment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='enrollment_set')
     barbell_class = models.ForeignKey(BarbellClass, on_delete=models.CASCADE)
     date_enrolled = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return f"{self.user.username} enrolled in {self.barbell_class.title}"

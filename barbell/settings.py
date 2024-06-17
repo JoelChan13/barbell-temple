04dc2f8419c10c1cbc4c6c8730fdd5e732fb3bca
Joel Chan <ic.chanjoel@gmail.com>
2024-06-17 18:11:13 +0000

    store sensitive data in env file

--- barbell/settings.py
+++ barbell/settings.py
@@ -22,12 +22,7 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*#of^w6nnm+fqi+rn$+ba-0*yspxk7pg=b5@+7jp5u13^gv*p1'
+SECRET_KEY = os.environ.get("django-insecure-*#of^w6nnm+fqi+rn$+ba-0*yspxk7pg=b5@+7jp5u13^gv*p1")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -87,13 +82,6 @@ WSGI_APPLICATION = 'barbell.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': BASE_DIR / 'db.sqlite3',
-#    }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse("postgres://uskt7tg0jea:7IylE7mGsyRz@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/armed_trek_knelt_458667")

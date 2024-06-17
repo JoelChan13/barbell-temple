79299a02d11a67d391f7c10a7899f56f876de531
Joel Chan <ic.chanjoel@gmail.com>
2024-06-17 18:31:33 +0000

    restored previous settings due to error in server

--- barbell/settings.py
+++ barbell/settings.py
@@ -22,7 +22,12 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.environ.get("django-insecure-*#of^w6nnm+fqi+rn$+ba-0*yspxk7pg=b5@+7jp5u13^gv*p1")
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-*#of^w6nnm+fqi+rn$+ba-0*yspxk7pg=b5@+7jp5u13^gv*p1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

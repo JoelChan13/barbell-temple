7f2e5e511b43c556bc147f1ec3b2751adc671546
Joel Chan <ic.chanjoel@gmail.com>
2024-06-20 10:04:26 +0000

    add default image when creating a user

--- users/models.py
+++ users/models.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)  # One-to-one relationship with User model
-    image = CloudinaryField('image', default='default.jpg', folder='profile_pictures')  # CloudinaryField for profile picture
+    image = CloudinaryField('image', default='https://res.cloudinary.com/dyvpfbw4j/image/upload/v1718875416/default.jpg', folder='profile_pictures')  # CloudinaryField for profile picture
 
     def __str__(self):
         return f'{self.user.username} Profile'
